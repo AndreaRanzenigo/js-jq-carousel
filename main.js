fa9ec1a1e83fb8b696595dc691bff999876c8230
Andrea <andre98377@gmail.com>
2020-10-27 16:06:13 +0100

    slider img keyboard

--- main.js
+++ main.js
@@ -1,38 +1,56 @@
 // Slide image 
+$(document).ready( function() {
 
-$('.next').click( function() {
-    navImage('next');
-})
+    //Evento con il click
+    $('.next').click( function() {
+        navImage('next');
+    })
+    
+    $('.prev').click(function() {
+        navImage('prev');
+    });
 
-$('.prev').click(function() {
-    navImage('prev');
-})
-
-
-//Navigazione avanti e indietro tra le immagini
-function navImage(direction) {
-    var activeImage = $('.images img.active');
-    var activeCircle = $('.nav i.active');
-
-    activeImage.removeClass('active');
-    activeCircle.removeClass('active');
-
-    if (direction === 'next') {
-        if (activeImage.hasClass('last') === true) {
-            $('.images img.first').addClass('active');
-            $('.nav i.first').addClass('active');
-        } else {
-            activeImage.next('img').addClass('active');
-            activeCircle.next('i').addClass('active');
+    //Navigazione Keyboard
+    $(document).keydown( function(event) {
+        //SX
+        if (event.keyCode == 37) {
+            navImage('prev');
+        } 
+        //DX
+        else if (event.keyCode == 39) {
+            navImage('next');
+        }
+    });
+    
+    
+    //Navigazione avanti e indietro tra le immagini
+    function navImage(direction) {
+        var activeImage = $('.images img.active');
+        var activeCircle = $('.nav i.active');
+    
+        activeImage.removeClass('active');
+        activeCircle.removeClass('active');
+    
+        //NEXT
+        if (direction === 'next') {
+            if (activeImage.hasClass('last') === true) {
+                $('.images img.first').addClass('active');
+                $('.nav i.first').addClass('active');
+            } else {
+                activeImage.next('img').addClass('active');
+                activeCircle.next('i').addClass('active');
+            }
+        }
+        //PREV
+        else if (direction === 'prev') {
+            if (activeImage.hasClass('first') === true) {
+                $('.images img.last').addClass('active');
+                $('.nav i.last').addClass('active');
+            } else {
+                activeImage.prev('img').addClass('active');
+                activeCircle.prev('i').addClass('active');
+            }
         }
     }
-    else if (direction === 'prev') {
-        if (activeImage.hasClass('first') === true) {
-            $('.images img.last').addClass('active');
-            $('.nav i.last').addClass('active');
-        } else {
-            activeImage.prev('img').addClass('active');
-            activeCircle.prev('i').addClass('active');
-        }
-    }
-}
+
+});
